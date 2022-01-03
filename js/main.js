53773239cf635f37c2e2950fa6885bdf268946f7
AmiReza <Mr.Ara1999@gmail.com>
2022-01-03 15:01:42 +0330

    offer-box

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 let slideIndex = 1;
+let remainingTime = 70000;
+function setTime(){
+    if(remainingTime == 0)return;
+    let h = Math.floor(remainingTime/3600);
+    let m = Math.floor((remainingTime/3600)%60);
+    let s = ((remainingTime%3600)%60);
+
+    document.querySelector('#second').innerHTML = s;
+    document.querySelector('#minutes').innerHTML = m;
+    document.querySelector('#hours').innerHTML = h;
+
+    
+}
+setInterval(()=>{
+remainingTime -= 1;
+setTime();
+},1000)
 function setSlides(input,index){
     slideIndex = index;
     let item = document.querySelector(`#${input}`);
